c6803a2986d60775d9f97260e6a093c4f8daab2d
Debora Rudzińska <deborar@gazeta.pl>
2019-08-02 13:41:48 +0200

    Add changes script.js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,4 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 
 let playerMove ='papier';
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
