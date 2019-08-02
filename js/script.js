c69b839073366757a2f09ab12cbee57a341eb5f9
Debora11 <53270174+Debora11@users.noreply.github.com>
2019-08-02 20:21:06 +0200

    remove all

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-let computerMove = `kamień`;
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-let playerMove ='papier';
-
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
